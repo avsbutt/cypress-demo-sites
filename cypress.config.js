1227eecb8466136ca5f11ec4ae1b847ff389ac0d
avsbutt <awais.3utt@gmail.com>
2024-03-05 17:02:57 +0500

    Add Cypress workflow configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  projectId: "fa2dwp",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
